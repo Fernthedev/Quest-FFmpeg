bd829e42e443cae7ee114522fbe16786d77d5691
EnderdracheLP <enderdrachelp@gmail.com>
2021-03-20 00:36:54 +0100

    Uploading ffmpeg build files from latest build

---
+++ sources/ffmpeg/ffmpeg-4.3.1/libavcodec/aarch64/idctdsp_init_aarch64.d
@@ -0,0 +1,14 @@
+libavcodec/aarch64/idctdsp_init_aarch64.o: \
+  libavcodec/aarch64/idctdsp_init_aarch64.c libavutil/attributes.h \
+  libavutil/cpu.h libavutil/arm/cpu.h libavutil/cpu_internal.h config.h \
+  libavcodec/avcodec.h libavutil/samplefmt.h libavutil/avutil.h \
+  libavutil/common.h libavutil/macros.h libavutil/version.h \
+  libavutil/avconfig.h libavutil/intmath.h libavutil/mem.h \
+  libavutil/error.h libavutil/internal.h libavutil/timer.h \
+  libavutil/log.h libavutil/aarch64/timer.h libavutil/dict.h \
+  libavutil/pixfmt.h libavutil/libm.h libavutil/intfloat.h \
+  libavutil/mathematics.h libavutil/rational.h libavutil/buffer.h \
+  libavutil/channel_layout.h libavutil/frame.h libavutil/hwcontext.h \
+  libavcodec/bsf.h libavcodec/codec_id.h libavcodec/codec_par.h \
+  libavcodec/packet.h libavcodec/version.h libavcodec/codec.h \
+  libavcodec/codec_desc.h libavcodec/idctdsp.h libavcodec/aarch64/idct.h
